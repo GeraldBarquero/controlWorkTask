74e7e82a09446ad6058ff16aa944e89cc55f9755
Gerald Barquero <gerald.bv1@gmail.com>
2019-01-25 17:34:44 -0600

    primer commit

---
+++ api/functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+import * as firebaseHelper from 'firebase-functions-helper';
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
